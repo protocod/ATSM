aa193b1d65ff2812afad5b3acd77a8e3142027ec
Hristo Ivanov <hristo.ivanov@circles.bz>
2019-12-05 17:18:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/atsm/MainActivity.java
@@ -0,0 +1,28 @@
+ package com.example.atsm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.atsm.ui.login.LoginActivity;
+
+ public class MainActivity extends AppCompatActivity {
+
+    public void showLoginActivity(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.topbar);
+        //setSupportActionBar(myToolbar);
+    }
+}
